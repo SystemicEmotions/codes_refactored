6b36a70309ded4b47328dbaf05b8edf6ea23c375
SystemicEmotions <alokmalakar20007@gmail.com>
2018-12-10 22:02:12 +0530

    Add files via upload

---
+++ find_element_with_different_freq.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int a[] = {1,1,2,2,3};
+	//int a[] = {0,1,2,4,4};
+	int n = sizeof(a)/sizeof(a[0]);
+
+	map<int, int> m ;
+
+	for(int i=0;i<n;i++){
+		m[a[i]]++;
+	}
+
+	map<int,int> ::iterator it; 
+	map<int, int> freq;
+
+	for(it=m.begin();it!=m.end();++it){
+		freq[it->second]++;
+	}
+
+	for(it=m.begin();it!=m.end();++it){
+		if(freq[it->second]==1)
+			cout<<it->first<<endl;
+	}
+	
+}
+
